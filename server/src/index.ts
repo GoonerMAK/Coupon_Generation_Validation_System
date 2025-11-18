e9efc84aeb6502b5a056cd8622645cdb6b29517d
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2025-11-18 11:51:00 +0600

    added backend setup

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+export const app = express()
+dotenv.config();
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT;
+export const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
